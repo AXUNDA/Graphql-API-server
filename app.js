e0f6b32b5947a680b87078d687dad26e6dccb6b3
Azunda <Charlesemmanuelazunda@gmail.com>
2023-10-24 07:28:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ const myschema = require("./schema/schema.js")
 const app = express();
 var cors = require('cors')
 app.use(cors())
-app.use("/", graphqlHTTP({
+app.use("/graphql", graphqlHTTP({
     schema: myschema,
     graphiql: true
 
 
 
 }));
+
+app.get('/',(req,res)=>{
+    res.send('hello world')
+})
 app.listen(process.env.PORT || 3000, function () {
     console.log('app is active')
 });
